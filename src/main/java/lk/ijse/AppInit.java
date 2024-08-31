d412cff09b833d1e3356a5d940a78f0ff205ce7d
ayomahansani <ayohansi17@gmail.com>
2024-08-31 10:07:46 +0530

    craeted configuration class

--- src/main/java/lk/ijse/AppInit.java
+++ src/main/java/lk/ijse/AppInit.java
@@ -1,8 +1,14 @@
 package lk.ijse;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
+
 public class AppInit {
     public static void main(String[] args) {
 
         var ctx = new AnnotationConfigApplicationContext();
+        ctx.register(Configuration.class);
+        ctx.refresh();
+        ctx.registerShutdownHook();
     }
 }
---
+++ src/main/java/lk/ijse/config/Configuration.java
@@ -0,0 +1,8 @@
+package lk.ijse.config;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@org.springframework.context.annotation.Configuration
+@ComponentScan("lk.ijse")
+public class Configuration {
+}
